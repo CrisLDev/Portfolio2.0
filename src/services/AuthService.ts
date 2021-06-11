0f8300e776be8ec350de96b9a6b246e9740210a7
CrisLDev <leonardomoreirapazmio@gmail.com>
2021-06-10 19:30:03 -0500

    Change for work with env variables

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -2,28 +2,28 @@ import axios from 'axios';
 import { SignInData, SignUpData } from '../interfaces/Auth';
 import { User } from '../interfaces/User';
 
-const API = process.env.API || 'http://localhost:4000/api/';
+const API = process.env.API || 'http://localhost:4000/api';
 
 export const getUsers = async () => {
-    return await axios.get<User[]>(`${API}user`);
+    return await axios.get<User[]>(`${API}/user`);
 }
 
 export const getUserById = async (id: string) => {
-    return await axios.get<User>(`${API}user/${id}`);
+    return await axios.get<User>(`${API}/user/${id}`);
 }
 
 export const signIn = async (data: SignInData, config: any) => {
-    return await axios.post<User>(`${API}auth`, data, config);
+    return await axios.post<User>(`${API}/auth`, data, config);
 }
 
 export const signUp = async (data: SignUpData, config: any) => {
-    return await axios.post<User>(`${API}users`, data, config);
+    return await axios.post<User>(`${API}/users`, data, config);
 }
 
 export const loggedUser = async () => {
-    return await axios.get<User>(`${API}auth`)
+    return await axios.get<User>(`${API}/auth`)
 }
 
 export const sendPasswordResetEmail = async (email: string) => {
-    return await axios.post<User>(`${API}reset`, email);
+    return await axios.post<User>(`${API}/reset`, email);
 }
--- src/services/TecnologyService.ts
+++ src/services/TecnologyService.ts
@@ -1,24 +1,24 @@
 import axios from 'axios';
 import { Tecnology } from '../interfaces/Tecnology';
 
-const API = process.env.API || 'http://localhost:4000/api/tecnology';
+const API = process.env.API || 'http://localhost:4000/api';
 
 export const getTecnologies = async () => {
-    return await axios.get<Tecnology[]>(`${API}`);
+    return await axios.get<Tecnology[]>(`${API}/tecnology`);
 }
 
 export const getTecnology = async (id: string) => {
-    return await axios.get<Tecnology>(`${API}/${id}`);
+    return await axios.get<Tecnology>(`${API}/tecnology/${id}`);
 }
 
 export const createTecnology = async (tecnology: Tecnology, config: any) => {
-    return await axios.post(`${API}`, tecnology, config);
+    return await axios.post(`${API}/tecnology`, tecnology, config);
 }
 
 export const editTecnology = async (id: string, tecnology: Tecnology, config: any) => {
-    return await axios.put(`${API}/${id}`, tecnology, config);
+    return await axios.put(`${API}/tecnology/${id}`, tecnology, config);
 }
 
 export const deleteTecnology = async (id: string | undefined) => {
-    return await axios.delete(`${API}/${id}`)
+    return await axios.delete(`${API}/tecnology/${id}`)
 }
